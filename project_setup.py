10e49f67517391d41c784d9e3297f57c2126bc47
knebiolo <61742537+knebiolo@users.noreply.github.com>
2020-03-08 21:43:15 -0400

    Delete project_setup.py

--- project_setup.py
+++
@@ -1,4 +0,0 @@
-import abtas
-proj_dir = r'C:\Users\Kevin Nebiolo\Desktop\Telemetry_Test'                   # what is the project directory?
-dbName = 'telemetry_test.db' 
-abtas.createTrainDB(proj_dir, dbName)  # create project database
