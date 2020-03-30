1cc774eb6a11214fd1883e5560951c45a3cfb086
knebiolo <kevin.nebiolo@kleinschmidtgroup.com>
2020-03-29 20:07:21 -0400

    putting together a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 19:48:31 2020
+
+@author: Kevin Nebiolo
+"""
+
+from setuptools import setup
+
+setup(name = 'biotas',
+      version = '0.1',
+      description = '''BIO-Telemetry Analysis Software (BIOTAS) for use in 
+      removing false positive and overlap detections from radio telemetry projects.''',
+      url = 'https://github.com/knebiolo/biotas',
+      author = 'Kevin P. Nebiolo',
+      author_email = 'kevin.nebiolo@kleinschmidtgroup.com',
+      license = 'MIT',
+      packages = ['biotas'],
+      zip_safe = False
+      )
