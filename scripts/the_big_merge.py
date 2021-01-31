62de207f6dd83f35120d6d5be1859df3253cba4b
tcastrosantos <tcastrosantos@usgs.gov>
2021-01-31 14:03:18 -0500

    Update the_big_merge.py
    Added call to 'the little merge()'  Struggling with a locked database issue in the to_sql() call toward the end

--- scripts/the_big_merge.py
+++ scripts/the_big_merge.py
@@ -4,18 +4,21 @@ import os
 import sqlite3
 
 # declare workspaces
-proj_dir = r'E:\Manuscript\CT_River_2015'                                      # what is the project directory?
-dbName = 'ctr_2015_v2.db'                                                    # what is the name of the project database
+proj_dir = r'C:\a\Projects\BCHydro\Data'                                      # what is the project directory?
+dbName = 'SiteC_2020.db'                                                    # what is the name of the project database
 outputWS = os.path.join(proj_dir,'Output')
 projectDB = os.path.join(proj_dir,'Data',dbName)
 
-rec_list = ['T13','T18','T21','T22']
+rec_list = ['F33a','F33b','F34','F35','F36a','F36b','F37','F38','F39','F40']
 
 # the big merge
-recaptures = biotas.the_big_merge(outputWS,projectDB, pre_release_Filter = True, rec_list = rec_list)
+#recaptures = biotas.the_big_merge(outputWS,projectDB, pre_release_Filter = True, rec_list = rec_list)
+recaptures = biotas.the_little_merge(outputWS,projectDB, pre_release_Filter = True, rec_list = rec_list)
+
+
+recaptures.to_sql('tblRecaptures',sqlite3.connect(projectDB),if_exists ='append')
 
 recaptures.to_csv(os.path.join(outputWS,'recaptures.csv'))
-recaptures.to_sql('tblRecaputres',sqlite3.connect(projectDB),if_exists ='append')
 
 rec_by_fish = recaptures.groupby(['FreqCode'])['recID'].unique()
 
