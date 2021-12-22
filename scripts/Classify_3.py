2f131f441bcafad8744f9e2ca2ee84b06872073b
Kevin P Nebiolo, PhD <61742537+knebiolo@users.noreply.github.com>
2021-12-21 20:40:04 -0500

    update to best practices

--- scripts/Classify_3.py
+++ scripts/Classify_3.py
@@ -3,43 +3,56 @@ import time
 import os
 import sqlite3
 import pandas as pd
-import biotas.biotas as biotas
-import warnings
-warnings.filterwarnings('ignore')
+import biotas
+
+#%% Part 1: Set Up Script Parameters
 tS = time.time()
+
 #set script parameters
 class_iter= 4 #Enter the iteration number here--start at 2
-site = 'T14'                                                                   # what is the site/receiver ID?
-recType = 'lotek'                                                              # what is the receiver type?
-proj_dir = r'D:\Manuscript\CT_River_2015'                                      # what is the project directory?
-dbName = 'ctr_2015_v2.db'                                                           # whad did you call the database?
+# what is the site/receiver ID?
+site = 'T14'   
+# what is the receiver type?                                                                
+recType = 'lotek' 
+# what is the project directory?                                                             
+proj_dir = r'D:\Manuscript\CT_River_2015'  
+# whad did you call the database?                                    
+dbName = 'ctr_2015_v2.db'                                                           
 
-# directory creations
-outputWS = os.path.join(proj_dir,'Output')                                 # we are getting time out error and database locks - so let's write to disk for now 
-outputScratch = os.path.join(outputWS,'Scratch')                           # we are getting time out error and database locks - so let's write to disk for now 
+# create directories with OS tools
+outputWS = os.path.join(proj_dir,'Output')                                  
+outputScratch = os.path.join(outputWS,'Scratch')                           
 workFiles = os.path.join(proj_dir,'Data','TrainingFiles')
 projectDB = os.path.join(proj_dir,'Data',dbName)
 figure_ws = os.path.join(proj_dir,'Output','Figures')
 
-# list fields used in likelihood classification, must be from this list:
+# A-la-carte likelihood, construct a model from the following parameters:
 # ['conRecLength','consDet','hitRatio','noiseRatio','seriesHit','power','lagDiff']
 fields = ['conRecLength','hitRatio','lagDiff','power','noiseRatio']
+
 # Do we want to use an informed prior?
 prior = False
+
 print ("Set Up Complete, Creating Histories")
+
+#%% Part 2: Classify Detections using A-La-Carte Likelihood and Summarize
+
 # get the fish to iterate through using SQL 
 conn = sqlite3.connect(projectDB)
 c = conn.cursor()
 sql = "SELECT FreqCode FROM tblRaw WHERE recID == '%s';"%(site)
 histories = pd.read_sql(sql,con = conn)
-tags = pd.read_sql("SELECT FreqCode, TagType FROM tblMasterTag WHERE TagType == 'Study'", con = conn)
-histories = histories.merge(right = tags, left_on = 'FreqCode', right_on = 'FreqCode')
+tags = pd.read_sql('''SELECT FreqCode, TagType 
+                   FROM tblMasterTag 
+                   WHERE TagType == 'Study' ''', con = conn)
+histories = histories.merge(right = tags, 
+                            left_on = 'FreqCode', 
+                            right_on = 'FreqCode')
 histories = histories[histories.TagType == 'Study'].FreqCode.unique()
 c.close()
 histories = histories.tolist()
 
 print ("There are %s fish to iterate through at site %s" %(len(histories),site))
-# create list of training data objects to iterate over with a Pool multiprocess
 
 # create training data for this round of classification
 train = biotas.create_training_data(site,projectDB, rec_list = [site])
@@ -47,16 +60,27 @@ train = biotas.create_training_data(site,projectDB, rec_list = [site])
 counter = 0
 for i in histories:
     counter = counter + 1
-    class_dat =biotas.classify_data(i,site,fields,projectDB,outputScratch,train,informed_prior = prior,reclass_iter=class_iter)
+    class_dat =biotas.classify_data(i,
+                                    site,
+                                    fields,
+                                    projectDB,
+                                    outputScratch,
+                                    train,
+                                    informed_prior = prior,
+                                    reclass_iter=class_iter)
     biotas.calc_class_params_map(class_dat)
     print ('classified detections for fish %s, %s percent complete'%(i,round(counter/len(histories),2)))
 print ("Detections classified!") 
 biotas.classDatAppend(site,outputScratch,projectDB,reclass_iter = class_iter)
 print ("process took %s to compile"%(round(time.time() - tS,3)))
-# get data you just classified, run some statistics and make some plots
 
+# get data you just classified, run some statistics and make some plots
 del train, class_dat
 
-class_stats = biotas.classification_results(recType,projectDB,figure_ws,rec_list=[site],reclass_iter = class_iter)
+class_stats = biotas.classification_results(recType,
+                                            projectDB,
+                                            figure_ws,
+                                            rec_list=[site],
+                                            reclass_iter = class_iter)
 class_stats.classify_stats()    
 
--- scripts/classify_2.py
+++ scripts/classify_2.py
@@ -1,49 +1,65 @@
-import sys
-sys.path.append(r"C:\a\Projects\Winooski\2020\Data\BIOTAS\biotas")
-
 # import modules required for function dependencies
 import time
 import os
 import sqlite3
 import pandas as pd
-import warnings
-warnings.filterwarnings('ignore')
 import biotas
 
-#set script parameters
-site = '102'                                                                   # what is the site/receiver ID?
-recType = 'orion'                                                              # what is the receiver type?
-proj_dir = r'C:\a\Projects\Winooski\2020\Data'                    # what is the project directory?
-dbName = 'Winooski_2020_102.db'                                                   # what is the name of the project database?
-t_DBName = 'Winooski_102_Trainer.db'                                                # what is the name of the training database?  We assume it is in the same directory
+#%% Part 1: Set Up Script Parameters
+
+# what is the site/receiver ID?
+site = '102' 
+# what is the site/reciever ID in the training database? 
+t_site = '102'
+# what is the receiver type?                                                                 
+recType = 'orion'
+# what is the project directory?                                                              
+proj_dir = r'C:\a\Projects\Winooski\2020\Data'                    
+# what is the name of the project database?
+dbName = 'Winooski_2020_102.db'                                                   
+# what is the name of the training database?  We assume it is in the same directory
+t_DBName = 'Winooski_102_Trainer.db'                                                
 
 # optional orion parameters if receivers used switching
 scanTime = 2.0
 channels = 1
-# even if you aren't using switching, fill in this dictionary with the antenna to reciever ID relationship
+# Dictionary that stores the Antenna to Receiver relationship
 ant_to_rec_dict = {1:'102'}
 
 # create worskspaces - you haven't changed the directory have you?
 trainingDB = os.path.join(proj_dir,'Data',t_DBName)
 
-outputWS = os.path.join(proj_dir,'Output')                                     # we are getting time out error and database locks - so let's write to disk for now
-outputScratch = os.path.join(outputWS,'Scratch')                           # we are getting time out error and database locks - so let's write to disk for now
+# create some directories using OS tools
+outputWS = os.path.join(proj_dir,'Output')                                     
+outputScratch = os.path.join(outputWS,'Scratch')                           
 figure_ws = os.path.join(outputWS,'Figures')
 workFiles = os.path.join(proj_dir,'Data','Training_Files')
 projectDB = os.path.join(proj_dir,'Data',dbName)
 
-# list fields used in likelihood classification, must be from this list:
+# A-la-carte likelihood, construct a model from the following parameters:
 # ['conRecLength','consDet','hitRatio','noiseRatio','seriesHit','power','lagDiff']
-fields = ['conRecLength','consDet','hitRatio','noiseRatio','seriesHit','power','lagDiff']
-files = os.listdir(workFiles)
-print ("There are %s files to iterate through"%(len(files)))
+fields = ['power','lagDiff','hitRatio']            
+
+# Do we want to use an informed prior?
+prior = True       
+print ("Set Up Complete, Creating Histories")
+
 tS = time.time()
 
-# if you are using receivers that do not employ antenna switching:
-biotas.telemDataImport(site,recType,workFiles,projectDB)
+#%% Part 2: Classify Detections using A-La-Carte Likelihood and Summarize
+# We first need to import data 
+biotas.telemDataImport(site,
+                       recType,
+                       workFiles,
+                       projectDB,
+                       scanTime = scanTime,
+                       channels = channels,
+                       ant_to_rec_dict = ant_to_rec_dict)
 
-# if you are using recievers that use swtiching:
-#biotas.telemDataImport(site,recType,workFiles,projectDB, scanTime = scanTime, channels = channels, ant_to_rec_dict = ant_to_rec_dict)
+# Now we create a training dataset using the separate training database
+# it is also possible to pass a list of receivers 
+train = biotas.create_training_data(t_site,
+                                    trainingDB)
 
 for i in ant_to_rec_dict:
     # get the fish to iterate through using SQL
@@ -53,30 +69,40 @@ for i in ant_to_rec_dict:
     sql = "SELECT FreqCode FROM tblRaw WHERE recID == '%s';"%(site)
     histories = pd.read_sql(sql,con = conn)
     tags = pd.read_sql("SELECT FreqCode FROM tblMasterTag WHERE TagType == 'Study'", con = conn)
-    histories = histories.merge(right = tags, left_on = 'FreqCode', right_on = 'FreqCode').FreqCode.unique()
+    histories = histories.merge(right = tags, 
+                                left_on = 'FreqCode', 
+                                right_on = 'FreqCode').FreqCode.unique()
     c.close()
     print ("There are %s fish to iterate through at site %s" %(len(histories),site))
     print ("This will take a while")
     print ("Grab a coffee, call your mother.")
+    
     # create list of training data objects to iterate over with a Pool multiprocess
-
     conn = sqlite3.connect(trainingDB)
     c = conn.cursor()
     sql = "SELECT * FROM tblTrain WHERE recID == '%s';"%(site)
     tblTrainDF = pd.read_sql(sql,con = conn)
     c.close()
 
-
-    iters = []
     for j in histories:
-        iters.append(biotas.classify_data(j,ant_to_rec_dict[i],fields,projectDB,outputScratch,training_data=tblTrainDF,training = trainingDB))
-    print ("Finished creating history objects")
-    for k in iters:
-        biotas.calc_class_params_map(k)
+        class_dat = biotas.classify_data(i,
+                                         site,
+                                         fields,
+                                         projectDB,
+                                         outputScratch,
+                                         training_data=train,
+                                         informed_prior = prior)
+        biotas.calc_class_params_map(class_dat)   
+        print ("Fish %s classified"%(i))
     print ("Detections classified!")
     biotas.classDatAppend(site,outputScratch,projectDB)
+    
     print ("process took %s to compile"%(round(time.time() - tS,3)))
+    
     # generate summary statistics for classification by receiver type
-    class_stats = biotas.classification_results(recType,projectDB,figure_ws,rec_list=[ant_to_rec_dict[i]])
+    class_stats = biotas.classification_results(recType,
+                                                projectDB,
+                                                figure_ws,
+                                                rec_list=[ant_to_rec_dict[i]])
     class_stats.classify_stats()
 
