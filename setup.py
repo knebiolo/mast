02001530c1991de274244f10313708c622628f61
Kevin P Nebiolo, PhD <61742537+knebiolo@users.noreply.github.com>
2024-03-18 11:00:09 -0400

    cleaning up
    cleaning up for upload to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name = 'biotas',
-      version = '0.0.10',
-      description = '''BIO-Telemetry Analysis Software (BIOTAS) for use in
-      removing false positive and overlap detections from radio telemetry projects.''',
-      url = 'https://github.com/knebiolo/biotas',
+setup(name = 'pymast',
+      version = '0.0.1',
+      description = '''Movement Analysis Software for Telemetry (MAST) for
+use in removing false positive and overlap detections from radio telemetry
+projects and assessing 1D movement patterns.''',
+      url = 'https://github.com/knebiolo/mast',
       author = 'Kevin P. Nebiolo and Theodore Castro-Santos',
       author_email = 'kevin.nebiolo@kleinschmidtgroup.com',
       license = 'MIT',
-      packages = ['biotas',],
+      packages = ['pymast',],
       python_requires= '>=3.5',
       install_requires=["numpy >= 1.17.4",
                         "pandas >= 0.25.3",
@@ -16,6 +17,7 @@ setup(name = 'biotas',
                         "statsmodels >= 0.10.1",
                         "networkx >= 2.2",
                         "scipy >= 1.7.1",
-                        "sklearn"],
+                        "sklearn",
+                        "h5py"],
       zip_safe = False
       )
