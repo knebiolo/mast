65baaa47f07bb076467378417779441974c817c0
Kevin P Nebiolo, PhD. <61742537+knebiolo@users.noreply.github.com>
2024-11-12 18:50:25 -0500

    Update mast_complete_project.py

--- scripts/mast_complete_project.py
+++ scripts/mast_complete_project.py
@@ -36,104 +36,104 @@ project = radio_project(project_dir,
 # project.new_db_version(os.path.join(project_dir,'Nuyakuk_v2.h5'))
 # ,presence,classified,trained
 
-#%%  import data
-rec_id = 'R11'
-rec_type = 'ares'
-#TODO - remove these directory arguments - the project is smart
-training_dir = os.path.join(project_dir,'Data',rec_id)
-db_dir = os.path.join(project_dir,'%s.h5'%(db_name))
-scan_time = 10.5 #10.5        
-channels = 2. #2
-antenna_to_rec_dict = {'1':rec_id}
+# #%%  import data
+# rec_id = 'R11'
+# rec_type = 'ares'
+# #TODO - remove these directory arguments - the project is smart
+# training_dir = os.path.join(project_dir,'Data',rec_id)
+# db_dir = os.path.join(project_dir,'%s.h5'%(db_name))
+# scan_time = 10.5 #10.5        
+# channels = 2. #2
+# antenna_to_rec_dict = {'1':rec_id}
 
-project.telem_data_import(rec_id,
-                          rec_type,
-                          training_dir,
-                          db_dir,
-                          scan_time,
-                          channels,
-                          antenna_to_rec_dict,
-                          True)
+# project.telem_data_import(rec_id,
+#                           rec_type,
+#                           training_dir,
+#                           db_dir,
+#                           scan_time,
+#                           channels,
+#                           antenna_to_rec_dict,
+#                           True)
 
-# undo import
-# project.undo_import(rec_id)
+# # undo import
+# # project.undo_import(rec_id)
 
-#%%  train data
-# set parameters and get a list of fish to iterate over
-rec_id = 'R11'
-rec_type = 'ares'
-fishes = project.get_fish(rec_id = rec_id)
+# #%%  train data
+# # set parameters and get a list of fish to iterate over
+# rec_id = 'R11'
+# rec_type = 'ares'
+# fishes = project.get_fish(rec_id = rec_id)
 
-# iterate over fish and train
-for fish in fishes:
-    project.train(fish, rec_id)
+# # iterate over fish and train
+# for fish in fishes:
+#     project.train(fish, rec_id)
 
-# generate summary statistics
-project.training_summary(rec_type, site = [rec_id])
+# # generate summary statistics
+# project.training_summary(rec_type, site = [rec_id])
 
-# undo training
-# project.undo_training(rec_id)
+# # undo training
+# # project.undo_training(rec_id)
 
-# %% classify data
+# # %% classify data
 
-# Set initial parameters
-rec_id = 'R11'
-rec_type = 'ares'
-threshold_ratio = 1  # 1.0 = MAP Hypothesis
-likelihood = ['hit_ratio','cons_length', 'noise_ratio', 'power', 'lag_diff'] # a-la carte likelihood, standard fields: ['hit_ratio', 'cons_length', 'noise_ratio', 'power', 'lag_diff']
+# # Set initial parameters
+# rec_id = 'R11'
+# rec_type = 'ares'
+# threshold_ratio = 1  # 1.0 = MAP Hypothesis
+# likelihood = ['hit_ratio','cons_length', 'noise_ratio', 'power', 'lag_diff'] # a-la carte likelihood, standard fields: ['hit_ratio', 'cons_length', 'noise_ratio', 'power', 'lag_diff']
 
-project.reclassify(project=project, rec_id=rec_id, threshold_ratio=threshold_ratio, 
-                    likelihood_model=likelihood, rec_type=rec_type, rec_list=None)
+# project.reclassify(project=project, rec_id=rec_id, threshold_ratio=threshold_ratio, 
+#                     likelihood_model=likelihood, rec_type=rec_type, rec_list=None)
 
-# undo classification 
-# project.undo_classification(rec_id)
+# # undo classification 
+# # project.undo_classification(rec_id)
 
-#%% cross validate
+# #%% cross validate
 
 
-#%% calculate bouts
-# get nodes
-node = 'R16'
+# #%% calculate bouts
+# # get nodes
+# node = 'R16'
 
-# create a bout object
-bout = pymast.bout(project, node, 2, 21600)
+# # create a bout object
+# bout = pymast.bout(project, node, 2, 21600)
     
-# Find the threshold
-threshold = bout.fit_processes()
+# # Find the threshold
+# threshold = bout.fit_processes()
     
-# calculate presences - or pass float
-bout.presence(threshold)
-# bout.presence(3600)
+# # calculate presences - or pass float
+# bout.presence(threshold)
+# # bout.presence(3600)
 
-# undo bouts
-# project.undo_bouts(node)
+# # undo bouts
+# # project.undo_bouts(node)
     
-#%% reduce overlap
-# create edges showing parent:child relationships for nodes in network
+# #%% reduce overlap
+# # create edges showing parent:child relationships for nodes in network
 
-# parents
-for i in project.receivers.index:
-    # children
-    for j in project.receivers.index:
-        if i != j:
-            edges = []
-            edges.append((i, j))
-            nodes = [i,j]
+# # parents
+# for i in project.receivers.index:
+#     # children
+#     for j in project.receivers.index:
+#         if i != j:
+#             edges = []
+#             edges.append((i, j))
+#             nodes = [i,j]
             
-            # create an overlap object and apply one of the nested doll algorithms
-            doll = pymast.overlap_reduction(nodes, edges, project)
+#             # create an overlap object and apply one of the nested doll algorithms
+#             doll = pymast.overlap_reduction(nodes, edges, project)
             
-            # doll.nested_doll() 
-            doll.unsupervised_removal()
+#             # doll.nested_doll() 
+#             doll.unsupervised_removal()
 
-#nodes = project.receivers.index
-nodes = project.receivers.index
+# #nodes = project.receivers.index
+# nodes = project.receivers.index
 
-#project.undo_overlap()
+# #project.undo_overlap()
 
-#%% create a recaptures table
-#project.undo_recaptures()
-project.make_recaptures_table(export = False) 
+# #%% create a recaptures table
+# #project.undo_recaptures()
+# project.make_recaptures_table(export = False) 
 
 #%% create Time to Event Model
     
@@ -147,7 +147,10 @@ node_to_state = {'R01':1,'R02':1,'R10':2,'R12':2,'R03':3,'R04':3}
 tte = formatter.time_to_event(node_to_state,
                               project,
                               initial_state_release = False,
-                              species = 'Pink')
+                              cap_loc = None,
+                              rel_loc = 'Kleinschmidt',
+                              species = 'Sockeye',
+                              rel_date = '01-01-2023')
 
 # Step 2, format data - without covariates
 tte.data_prep(project, 
